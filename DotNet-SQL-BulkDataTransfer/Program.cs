8055c29f09e1dbae3889dc2ec3193e97797842ab
Ryan Cope <rcope@1800radiator.com>
2023-09-17 01:38:32 -0700

    Update Program.cs

--- DotNet-SQL-BulkDataTransfer/Program.cs
+++ DotNet-SQL-BulkDataTransfer/Program.cs
@@ -1,26 +1,15 @@
 ﻿
 using SQL.BulkDataTransfer.Services;
 
-///https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy.destinationtablename?view=dotnet-plat-ext-7.0
-///https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy.enablestreaming?view=dotnet-plat-ext-7.0https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy?view=dotnet-plat-ext-7.0
-///https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy?view=dotnet-plat-ext-7.0#examples
-///https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlbulkcopy?view=dotnet-plat-ext-7.0
-///https://www.sentryone.com/blog/using-sqlbulkcopy-net-faster-bulk-data-loading
-
 namespace SQL.BulkDataTransfer
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            // See https://aka.ms/new-console-template for more information
-            Console.WriteLine("Hello, World!");
-
-            //Get transfer queue from config JSON
+            //Parse json config, initialize list of DbTransfer models for iteration
             foreach(var transfer in ConfigService.GetTransfers())
             {
-
 
                 Console.WriteLine(string.Format("{0} - Starting transfer for {1}..", DateTime.Now, transfer.source.objectName));
 
@@ -28,8 +17,6 @@ namespace SQL.BulkDataTransfer
                 DbService.BulkTransfer(transfer.source, transfer.destination);
 
                 Console.WriteLine(string.Format("{0} - Starting transfer for {1}..", DateTime.Now, transfer.source.objectName));
-                
-
 
             }
 
